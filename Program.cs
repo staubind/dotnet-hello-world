451f12b779992a6e944acf908eeed9516737cf8c
Dan St. Aubin <staubindj@gmail.com>
2021-09-20 15:56:22 -0500

    c# up and running, first project created and run. hello, world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
