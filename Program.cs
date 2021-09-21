131dce326de07b2c2eff033cbf5aee6459fc49e7
Dan St. Aubin <staubindj@gmail.com>
2021-09-20 22:40:29 -0500

    finished a few code wars on basic c#. Man, theres a ton to learn here. looks like a steeper learning curve than js or java or python

--- Program.cs
+++ Program.cs
@@ -6,7 +6,54 @@ namespace dotnet_hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Opposites attract:
+            public class LoveDetector
+            {
+                public static bool lovefunc(int flower1, int flower2)
+                { 
+                    //Moment of truth...
+                  if (flower1 % 2 != flower2 % 2) {
+                    return true;
+                  } else {
+                    return false;
+                  }
+                }
+            }
+
+            // It only takes one...
+            public class Kata
+            {
+              public static bool Check(object[] a, object x)
+              {
+              
+                foreach(var element in a) {
+                  if (element.ToString() == x.ToString()) {
+                    return true;
+                  }
+                }
+                return false;
+
+                }
+              }
+
+                // sum of positive
+                using System;
+                using System.Linq;
+
+                public class Kata
+                {
+                  public static int PositiveSum(int[] arr)
+                  {
+                    var sum = 0;
+                    foreach(var num in arr) {
+                      if (num > 0) {
+                        sum += num;
+                      }
+                    }
+                    return sum;
+                  }
+                }
+
         }
     }
 }
